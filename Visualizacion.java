6898eb9b4a08451a30fe330750d1be03f064bd99
alexsachinas <158860586+alexsachinas@users.noreply.github.com>
2024-02-20 18:37:38 -0600

    Visualizacion.java

---
+++ Visualizacion.java
@@ -0,0 +1,27 @@
+package com.mycompany.youtube.com;
+
+public class Visualizacion {
+      private Usuario espectador;
+    private Video video;
+
+    public Visualizacion(Usuario espectador, Video video) {
+        this.espectador = espectador;
+        this.video = video;
+    }
+
+    public Usuario getEspectador() {
+        return espectador;
+    }
+
+    public void setEspectador(Usuario espectador) {
+        this.espectador = espectador;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    } 
+}
